36ab360e56cecf7b8f9af09457558b7b8be11500
Felix Georgeault <fgeorgea@fu-r1-p3.s19.be>
2022-10-20 19:09:40 +0200

    first commit add files

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgeorgea <fgeorgea@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 19:06:24 by fgeorgea          #+#    #+#             */
+/*   Updated: 2022/10/20 19:08:25 by fgeorgea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+#endif
